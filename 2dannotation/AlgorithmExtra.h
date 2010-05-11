e0237654972438fe1d36ad4048be122d2db1f7d4
Marc-Frédérick Blanchet <marc.frederick.blanchet@gmail.com>
2010-05-11 13:10:02 +0000

    Added a tuple3 class, representing tuple of 3 element.  Comparison behavior is same as a std::pair<std::pair<class T, class U>, class V>.

--- 2dannotation/AlgorithmExtra.h
+++ 2dannotation/AlgorithmExtra.h
@@ -13,70 +13,70 @@ namespace annotate
 	{
 		std::set< T > setInter;
 		std::insert_iterator< std::set< T > > iter( setInter, setInter.begin() );
-	
+
 		std::set_intersection( set1.begin(), set1.end(),
 	          set2.begin(), set2.end(),
 	          iter );
-	
+
 		return setInter;
 	};
-	
+
 	template < class T >
 	std::set< T > SetIntersection( const std::set< T > & set1, const std::set< T > & set2 )
 	{
 		std::set< T > setInter;
 		std::insert_iterator< std::set< T > > iter( setInter, setInter.begin() );
-	
+
 		std::set_intersection( set1.begin(), set1.end(),
 	          set2.begin(), set2.end(),
 	          iter );
-	
+
 		return setInter;
 	};
-	
+
 	//----------------------------------------------------------------------
 	template < class T >
 	std::set< T > SetUnion( const std::set< T > & set1, const std::set< T > & set2 )
 	{
 		std::set< T > setUnion;
 		std::insert_iterator< std::set< T > > iter( setUnion, setUnion.begin() );
-	
+
 		std::set_union( set1.begin(), set1.end(),
 	          set2.begin(), set2.end(),
 	          iter );
-	
+
 		return setUnion;
 	};
-	
+
 	//----------------------------------------------------------------------
 	template < class T >
 	std::set< T > SetSymmetricDifference( const std::set< T > & set1, const std::set< T > & set2 )
 	{
 		std::set< T > setSymDiff;
 		std::insert_iterator< std::set< T > > iter( setSymDiff, setSymDiff.begin() );
-	
+
 		std::set_symmetric_difference( set1.begin(), set1.end(),
 	          set2.begin(), set2.end(),
 	          iter );
-	
+
 		return setSymDiff;
 	};
-	
-		
+
+
 	//----------------------------------------------------------------------
 	template < class T >
 	std::set< T > SetDifference( const std::set< T > & set1, const std::set< T > & set2 )
 	{
 		std::set< T > setDiff;
 		std::insert_iterator< std::set< T > > iter( setDiff, setDiff.begin() );
-	
+
 		std::set_difference( set1.begin(), set1.end(),
 	        set2.begin(), set2.end(),
 	        iter );
-	
+
 		return setDiff;
 	};
-	
+
 	template <class InputIterator1, class InputIterator2>
   	bool set_intersects ( InputIterator1 first1, InputIterator1 last1,
                           InputIterator2 first2, InputIterator2 last2)
@@ -92,15 +92,15 @@ namespace annotate
 		    {
 		    	 ++first2;
 		    }
-		    else 
-		    { 
+		    else
+		    {
 		    	bIntersects = true;
 		    	break;
 		    }
 		}
 		return bIntersects;
 	}
-	
+
 	template <class InputIterator1, class InputIterator2, class Compare>
   	bool set_intersects ( InputIterator1 first1, InputIterator1 last1,
                           InputIterator2 first2, InputIterator2 last2,
@@ -117,30 +117,95 @@ namespace annotate
 		    {
 		    	 ++first2;
 		    }
-		    else 
-		    { 
+		    else
+		    {
 		    	bIntersects = true;
 		    	break;
 		    }
 		}
 		return bIntersects;
 	}
-	
+
 	//----------------------------------------------------------------------
-	template <class T> struct less_ptr : std::binary_function <T,T,bool> 
+	template <class T> struct less_ptr : std::binary_function <T,T,bool>
 	{
   		bool operator() (const T* x, const T* y) const
     	{
     		return *x < *y;
     	}
 	};
-	
-	template <class T> struct less_auto_ptr : std::binary_function <T,T,bool> 
+
+	template <class T> struct less_auto_ptr : std::binary_function <T,T,bool>
 	{
   		bool operator() (const std::auto_ptr<T>& x, const std::auto_ptr<T>& y) const
     	{
     		return (*x) < (*y);
     	}
+	};
+
+	template <class T1, class T2, class T3> class tuple3
+	{
+	public:
+		typedef T1 first_type;
+		typedef T2 second_type;
+		typedef T3 third_type;
+
+		T1 first;
+		T2 second;
+		T3 third;
+
+		tuple3() : first(T1()), second(T2()), third(T3()) {}
+		tuple3(const T1& x, const T2& y, const T3& z) : first(x), second(y), third(z) {}
+		template <class U, class V, class W>
+			tuple3(const tuple3<U,V,W> &t)
+			: 	first(t.first),
+				second(t.second),
+				third(t.third) {}
+		bool operator ==(const tuple3& t) const
+		{
+			return ((first == t.first) && (second == t.second) && (third == t.third));
+		}
+		bool operator !=(const tuple3& t) const
+		{
+			return !((first == t.first) && (second == t.second) && (third == t.third));
+		}
+
+		bool operator <(const tuple3& t) const
+		{
+			return ((first < t.first)
+					|| ((first == t.first)
+						&& ((second < t.second) || ((second == t.second) && third < t.third))
+						)
+					);
+		}
+		bool operator >(const tuple3& t) const
+		{
+			return ((t.first < first)
+					|| ((t.first == first)
+						&& ((t.second < second) || ((t.second == second) && t.third < third))
+						)
+					);
+		}
+		bool operator <=(const tuple3& t) const
+		{
+			return ((first < t.first)
+					|| ((first == t.first)
+						&& ((second < t.second)
+								|| (((second == t.second) && third < t.third))
+									|| (third == t.third))
+						)
+					);
+		}
+		bool operator >=(const tuple3& t) const
+		{
+			return ((t.first < first)
+					|| ((t.first == first)
+						&& ((t.second < second)
+								|| (((t.second == second) && t.third < third))
+									|| (third == t.third))
+						)
+					);
+		}
 	};
 }
 
