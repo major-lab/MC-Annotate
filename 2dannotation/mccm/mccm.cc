8644ecc1981049c538e0cfe6fc15be8d3e807253
Marc-Frédérick Blanchet <marc.frederick.blanchet@gmail.com>
2010-05-11 14:01:41 +0000

    Modified to use CycleInfoFile instead of doing the processing explicitely.

--- 2dannotation/mccm/mccm.cc
+++ 2dannotation/mccm/mccm.cc
@@ -18,6 +18,7 @@
 #include "StringUtil.h"
 
 #include "CycleInfo.h"
+#include "CycleInfoFile.h"
 
 #include "mccore/Messagestream.h"
 #include "mccore/Version.h"
@@ -113,96 +114,12 @@ void read_options (int argc, char* argv[])
 	}
 }
 
-std::list<std::string> getResidues(const std::string& aResidues)
+std::set<annotate::CycleInfo> readCyclesFile(const std::string& astrFile)
 {
-	std::list<std::string> residues;
-	std::string strResidues = aResidues;
+	annotate::CycleInfoFile infile;
+	infile.read(astrFile);
 
-	annotate::cleanString(strResidues, ' ');
-	while(0 < strResidues.size())
-	{
-		std::string strRes;
-		std::size_t sep = strResidues.rfind('-');
-		if(sep != std::string::npos)
-		{
-			strRes = strResidues.substr(sep + 1, strResidues.size() - (sep + 1));
-			strResidues.erase(sep);
-		}
-		else
-		{
-			strRes = strResidues;
-			strResidues.clear();
-		}
-		residues.push_front(strRes);
-	}
-	return residues;
-}
-
-std::vector<std::vector<std::string> > getStrandResidues(
-	const std::string& aResidues,
-	const annotate::CycleProfile& aProfile)
-{
-	std::list<std::string> residues = getResidues(aResidues);
-	std::vector<std::vector<std::string> > strandResidues;
-
-	std::list<std::string>::const_iterator itRes = residues.begin();
-	std::list<unsigned int>::const_iterator itProf;
-	for(itProf = aProfile.strandProfile().begin();
-		itProf != aProfile.strandProfile().end();
-		++ itProf)
-	{
-		std::vector<std::string> strand;
-		unsigned int iRes = 0;
-		for(iRes = 0; iRes < *itProf; ++ iRes)
-		{
-			strand.push_back(*itRes);
-			++ itRes;
-		}
-		strandResidues.push_back(strand);
-		strand.clear();
-	}
-	return strandResidues;
-}
-
-std::set<annotate::CycleInfo> readCyclesFile(const std::string& aFile)
-{
-	unsigned int i = 1;
-	std::ifstream infile;
-	std::set<annotate::CycleInfo> infos;
-	infile.open (aFile.c_str(), std::ifstream::in);
-	std::string strLine;
-	std::vector<std::string> fields;
-
-	while(std::getline(infile, strLine).good())
-	{
-		annotate::cleanString(strLine, ' ');
-
-		fields = annotate::splitStringFields(strLine, ":");
-
-		std::string strPDBFile = fields[0];
-		unsigned int uiModel = atol(fields[1].c_str());
-		std::string strProfile = fields[2];
-		std::string strPredProfile = fields[3];
-		std::string strResIds = fields[4];
-		std::string strSeq = fields[5];
-
-		annotate::CycleProfile prof(strProfile);
-		annotate::CycleProfile predProf(strPredProfile);
-
-		annotate::CycleInfo::residue_profile resProfile;
-		resProfile = getStrandResidues(strResIds, prof);
-
-		std::vector<std::string> residues = annotate::splitStringFields(strSeq, "-");
-		annotate::CycleInfo cInfo(strPDBFile, uiModel, predProf, prof, resProfile, residues);
-
-		infos.insert(cInfo);
-		assert(infos.size() == i);
-		++ i;
-	}
-
-	infile.close();
-
-	return infos;
+	return infile.cycles();
 }
 
 std::string getModelString(const annotate::CycleInfo& aCycle)
@@ -215,8 +132,8 @@ std::string getModelString(const annotate::CycleInfo& aCycle)
 std::string getResIdsString(const annotate::CycleInfo& aCycle)
 {
 	std::ostringstream oss;
-	std::vector<std::string> resIds = aCycle.getResIds();
-	std::vector<std::string>::const_iterator it;
+	std::vector<mccore::ResId> resIds = aCycle.getResIds();
+	std::vector<mccore::ResId>::const_iterator it;
 	for(it = resIds.begin(); it != resIds.end(); ++ it)
 	{
 		if(it != resIds.begin())
