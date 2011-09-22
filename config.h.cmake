4e4441120a532476e80c84d2948aac252cbb1a36
Jonathan Roy <jonathan.roy.1@umontreal.ca>
2011-09-22 13:04:03 +0000

    Fix the license

--- config.h.cmake
+++ config.h.cmake
@@ -1,21 +1,9 @@
 /* This file is part of mcannotate.
  *
- * mcsym is a computer program that predicts all-atoms RNA tertiary structure from 
- * sequence and secondary structure
+ * mcannotate is an annotation tools for PDB files that produce information about nucleotides 
+ * conformation and their spatials interfactions
  * Copyright (C) 2008,2009,2010,2011 Institut de recherche en immunologie et en cancérologie
  *
- * mcannotate is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * mcannotate is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with mcannotate.  If not, see <http://www.gnu.org/licenses/>.
  */  
 
 #ifndef CONFIG_H
